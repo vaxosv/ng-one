c409d89cc8b691e5b8820fa7a7a04912f2677f3f
vaxosv <vaxo.sv@gmail.com>
2018-08-16 21:52:16 +0400

    add

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { Hero } from '../heroes/hero';
 
 @Component({
   selector: 'app-dashboard',
